42b75371cc6ee8bbbc72ea0a528d8f92955d9202
Yang Zhang <zocean636@gmail.com>
2021-03-16 02:24:37 -0400

    fix

--- source/conf.py
+++ source/conf.py
@@ -77,8 +77,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = 'nb_theme'
+html_theme = 'alabaster'
+#html_theme = 'nb_theme'
 html_theme_path = ["."]
 
 # Theme options are theme-specific and customize the look and feel of a theme
